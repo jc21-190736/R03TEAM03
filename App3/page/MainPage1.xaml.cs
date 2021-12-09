7c2b055d4e938e4c022c4cb556313c1f04f6e225
龍成 小林 <190736@jc-21.jp>
2021-12-09 15:52:24 +0900

    タブ追加

--- App3/page/MainPage1.xaml.cs
+++ App3/page/MainPage1.xaml.cs
@@ -1,8 +1,16 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace App3.page
 
 {
+   [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage1 : ContentPage
     {
         public MainPage1()
